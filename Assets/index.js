ef0be73219d9ad776ef9c3d31ba119e53056dec0
Ariana Winters <arianaw15@gmail.com>
2021-02-11 20:21:20 -0700

    Added contact page and fixed home page

--- Assets/index.js
+++ Assets/index.js
@@ -4,6 +4,20 @@ var aboutMe= $(".aboutMe");
 var resumeImg=$(".resumeImg")
 var aboutImg=$(".aboutImg")
 var projectImg=$(".projectImg")
+var contact =$(".contact-info")
+var iframe= $("iframe")
+
+function homePage(){
+        profilePic.attr("src", "Assets/profile-pic.jpg");
+        resumePdf.attr("src","");
+        aboutMe.addClass("aboutMe");
+        aboutMe.text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
+        resumeImg.addClass("hide");
+        aboutImg.removeClass("hide");
+        projectImg.addClass("hide");
+        contact.addClass("hide");
+    };
+homePage();
 
 function home(){
     $("#home-tab").on("click",function(){
@@ -16,6 +30,7 @@ function home(){
         resumeImg.addClass("hide");
         aboutImg.removeClass("hide");
         projectImg.addClass("hide");
+        contact.addClass("hide");
     })
     console.log("Button works")
     
@@ -31,12 +46,13 @@ function resume(){
         aboutImg.addClass("hide")
         resumeImg.removeClass("hide");
         projectImg.addClass("hide");
+        contact.addClass("hide");
     })
 }
 resume();
 
 function projects(){
-    $("#contact-tab").on("click", function(){
+    $("#project-tab").on("click", function(){
         $(".header").text("Projects");
         profilePic.attr("src", "");
         resumePdf.attr("src","");
@@ -44,6 +60,22 @@ function projects(){
         resumeImg.addClass("hide");
         aboutImg.addClass("hide");
         projectImg.removeClass("hide");
+        contact.addClass("hide");
     })
 }
 projects();
+
+function contactMe(){
+    $("#contact-tab").on("click",function(){
+        $(".header").text("Contact Me");
+        profilePic.attr("src", "");
+        resumePdf.attr("src","");
+        contact.removeClass("hide");
+        aboutMe.removeClass("aboutMe");
+        resumeImg.addClass("hide");
+        aboutImg.addClass("hide");
+        projectImg.addClass("hide");
+
+    })
+}
+contactMe();
